9329a4e65fef2cbb2cc22e15b7cac9a2fb072b2b
HyoungRok <167053162+HyoungRokRHee@users.noreply.github.com>
2025-12-10 15:23:49 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { NavermapsProvider } from 'react-naver-maps';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import MainPage from './pages/MainPage'; 
+import LoginSuccessPage from './pages/LoginSuccessPage'; // <-- import 추가
 import MapPage from './pages/MapPage'; 
 import WelfarePage from './pages/WelfarePage'; 
 
@@ -19,6 +20,10 @@ function App() {
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/signup" element={<SignUpPage />} />
                     <Route path="/main" element={<MainPage />} />
+                    
+                    {/* 👇 새로 추가된 경로 👇 */}
+                    <Route path="/loginsuccess" element={<LoginSuccessPage />} /> 
+                    <Route path="/option1" element={<div>Option 1 Page</div>} /> 
                     
                     <Route path="/map" element={<MapPage />} />
                     <Route path="/welfare" element={<WelfarePage />} />
