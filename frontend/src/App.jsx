49ab35ee23a2207ed25923198aa41419c7be1e95
HyoungRok <167053162+HyoungRokRHee@users.noreply.github.com>
2025-12-10 15:50:38 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import { NavermapsProvider } from 'react-naver-maps';
 
+// ... (기존 import 유지)
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import MainPage from './pages/MainPage'; 
 import LoginSuccessPage from './pages/LoginSuccessPage';
-
-// 👇 Option Pages import 추가 👇
 import Option1Page from './pages/Option1Page'; 
 import Option2Page from './pages/Option2Page';
 import Option3Page from './pages/Option3Page';
 import Option4Page from './pages/Option4Page';
 import Option5Page from './pages/Option5Page';
 import Option6Page from './pages/Option6Page';
+
+// 👇 새로 추가된 Pages import 👇
+import LoadingPage from './pages/LoadingPage'; 
+import Busan1Page from './pages/Busan1Page';
 
 import MapPage from './pages/MapPage'; 
 import WelfarePage from './pages/WelfarePage'; 
@@ -31,13 +34,16 @@ function App() {
                     <Route path="/main" element={<MainPage />} />
                     <Route path="/loginsuccess" element={<LoginSuccessPage />} /> 
                     
-                    {/* 👇 Option Pages 경로 추가 👇 */}
                     <Route path="/option1" element={<Option1Page />} />
                     <Route path="/option2" element={<Option2Page />} />
                     <Route path="/option3" element={<Option3Page />} />
                     <Route path="/option4" element={<Option4Page />} />
                     <Route path="/option5" element={<Option5Page />} />
                     <Route path="/option6" element={<Option6Page />} />
+                    
+                    {/* 👇 새로운 경로 추가 👇 */}
+                    <Route path="/loading" element={<LoadingPage />} />
+                    <Route path="/busan1" element={<Busan1Page />} />
                     
                     <Route path="/map" element={<MapPage />} />
                     <Route path="/welfare" element={<WelfarePage />} />
