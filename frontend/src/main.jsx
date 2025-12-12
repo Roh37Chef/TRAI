932642629c8f255548591404c433186f727b933e
HyoungRok <167053162+HyoungRokRHee@users.noreply.github.com>
2025-12-12 09:26:00 +0900

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,15 @@
-// src/main.jsx (최종 - MapProvider 추가)
+// src/main.jsx (최종 - MapProvider 제거 후 복구)
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { MapProvider } from 'react-naver-maps'; 
+// MapProvider import를 제거합니다.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 👇 전체 앱을 MapProvider로 감싸줍니다. (지도 오류 해결) */}
-    <MapProvider> 
-        <App />
-    </MapProvider>
+    {/* MapProvider를 제거합니다. */}
+    <App />
   </React.StrictMode>,
 )
